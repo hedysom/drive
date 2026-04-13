6b43b72b4f7d40168ca0dc08e1f7131afdc67976
hedysom <yevgeniy.terentyev@gmail.com>
2026-04-13 22:09:14 +0200

    env

--- src/env.js
+++ src/env.js
@@ -12,11 +12,11 @@ export const env = createEnv({
       .enum(["development", "test", "production"])
       .default("development"),
 
-    SINGLESTORE_USER: z.string(),
-    SINGLESTORE_PASS: z.string(),
-    SINGLESTORE_HOST: z.string(),
-    SINGLESTORE_PORT: z.string(),
-    SINGLESTORE_DB_NAME: z.string(),
+      SINGLESTORE_USER: z.string(),
+      SINGLESTORE_PASS: z.string(),
+      SINGLESTORE_HOST: z.string(),
+      SINGLESTORE_PORT: z.string(),
+      SINGLESTORE_DB_NAME: z.string(),
   },
 
   /**
@@ -35,12 +35,12 @@ export const env = createEnv({
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
+
     SINGLESTORE_USER: process.env.SINGLESTORE_USER,
     SINGLESTORE_PASS: process.env.SINGLESTORE_PASS,
     SINGLESTORE_HOST: process.env.SINGLESTORE_HOST,
     SINGLESTORE_PORT: process.env.SINGLESTORE_PORT,
     SINGLESTORE_DB_NAME: process.env.SINGLESTORE_DB_NAME,
-
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
