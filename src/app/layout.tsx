931928c96df155167a4da324f79322dea6071c35
hedysom <yevgeniy.terentyev@gmail.com>
2026-04-19 21:36:22 +0200

    lint?

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
-import "~/styles/globals.css";
+import "../styles/globals.css";
 
-import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
+import { Geist } from "next/font/google";
 
 export const metadata: Metadata = {
   title: "Create T3 App",
@@ -9,12 +9,17 @@ export const metadata: Metadata = {
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
+const geist = Geist({
+  subsets: ["latin"],
+  variable: "--font-geist-sans",
+});
+
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en" className={`${GeistSans.variable}`}>
-      <body>{children}</body>
+    <html lang="en" className={`${geist.variable}`}>
+      <body className="font-sans antialiased">{children}</body>
     </html>
   );
 }
